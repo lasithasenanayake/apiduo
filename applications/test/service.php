ca0d4b743c26fe11f2aa148f38200c3a3e69dbda
Lasitha Senanayake <lasitha.senanayake@gmail.com>
2019-03-20 19:03:49 +0530

    add ... alll

--- applications/test/service.php
+++ applications/test/service.php
@@ -10,46 +10,10 @@ class SearchServices {
         return $dbobj;
     }
 
-    public function postq($req){
+    public function gettest($req){
         
         $sall=$req->Body(true);
-        $f=new stdClass();
-        foreach($sall as $s){
-            $user=null;
-            if(isset($req->headers()->rhost)){
-                $user= Auth::AutendicateDomain($req->headers()->rhost,$req->headers()->sosskey,$s->storename,"query");
-                if(!isset($user->userid)){
-                    $err=new stdClass();
-                    $err->Mesaage="Error Authendicating User Autherize to this object store";
-                    $f->{$s->storename}=$err;
-                    continue;
-                }
-            }
-            $result= CacheData::getObjects(md5($s->search),$s->storename);
-            if(!isset($result)){
-                $result=null;
-                if($s->search!=""){
-                    $result = SOSSData::Query ($s->storename,urlencode($s->search));
-                }else{
-                    $result = SOSSData::Query ($s->storename,null);
-                }
-                if($result->success){
-                    $f->{$s->storename}=$result->result;
-                    if(isset($result->result)){
-                        if($s->search==""){
-                            $s->search="all";
-                        }
-                        CacheData::setObjects(md5($s->search),$s->storename,$result->result);
-                    }
-                }else{
-                    $f->{$s->storename}=$result;
-                }
-            }else{
-                $f->{$s->storename}= $result;
-            }
-            
-        }
-        return $f;
+        return $sall;
     }
 
     public function getattribute($req){
