7c4ab6046e7916582bccd72d1e409af229954ab1
Supun Dissanayake <supuncodes@gmail.com>
2019-03-22 00:11:33 +0530

    mssql helper created

---
+++ lib/mssql_helper.php
@@ -0,0 +1,24 @@
+<?php
+
+class MsSqlHelper {
+
+    private function getUnit(){
+
+    }
+    
+    public function insert(){
+
+    }
+
+    public function update(){
+
+    }
+
+    public function execute(){
+
+    }
+
+    public function query(){
+
+    }
+}
